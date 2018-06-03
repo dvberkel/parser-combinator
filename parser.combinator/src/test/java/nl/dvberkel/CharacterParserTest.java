c62f429564306f92c0b9564ba851c2b2f1503ac5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-06-03 07:35:19 +0200

    Make ParseResult subclasses inner classes of ParseResult

--- parser.combinator/src/test/java/nl/dvberkel/CharacterParserTest.java
+++ parser.combinator/src/test/java/nl/dvberkel/CharacterParserTest.java
@@ -6,20 +6,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 
-public class CharacterParserTest{
+public class CharacterParserTest {
 
     @Test
-    public void should_parse_a_single_character()
-    {
-        Parser<String, Character> parser =  character('A');
+    public void should_parse_a_single_character() {
+        Parser<String, Character> parser = character('A');
         ParseResult<Character> result = parser.parse("ABC");
         assertEquals(ParseResult.Ok(Character.valueOf('A'), "BC"), result);
     }
 
     @Test
-    public void should_parse_a_single_character_if_characters_match()
-    {
-        Parser<String, Character> parser =  character('A');
+    public void should_parse_a_single_character_if_characters_match() {
+        Parser<String, Character> parser = character('A');
         ParseResult<Character> result = parser.parse("BC");
         assertEquals(ParseResult.Error("Expected character 'A'", "BC"), result);
     }
@@ -30,19 +28,20 @@ interface Parser<I, O> {
 }
 
 class CharacterParser implements Parser<String, Character> {
-    public static CharacterParser character(char character){
+    public static CharacterParser character(char character) {
         return new CharacterParser(character);
     }
+
     private final char expectedCharacter;
 
-    public CharacterParser(char character){
+    public CharacterParser(char character) {
         this.expectedCharacter = character;
     }
 
     @Override
     public ParseResult<Character> parse(String input) {
         if (input.startsWith("A")) {
-            return ParseResult.Ok(Character.valueOf('A'),"BC");
+            return ParseResult.Ok(Character.valueOf('A'), "BC");
         } else {
             return ParseResult.Error("Expected character 'A'", "BC");
         }
@@ -57,60 +56,60 @@ abstract class ParseResult<O> {
     public static <P> ParseResult<P> Error(String message, String remainingInput) {
         return new Error(message, remainingInput);
     }
-}
 
-class Ok<O> extends ParseResult<O> {
-    private final O result;
-    private final String input;
+    private static class Ok<O> extends ParseResult<O> {
+        private final O result;
+        private final String input;
 
-    public Ok(O result, String remainingInput){
-        this.result = result;
-        this.input = remainingInput;
+        public Ok(O result, String remainingInput) {
+            this.result = result;
+            this.input = remainingInput;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Ok<?> that = (Ok<?>) o;
+
+            if (!result.equals(that.result)) return false;
+            return input.equals(that.input);
+        }
+
+        @Override
+        public int hashCode() {
+            int result1 = result.hashCode();
+            result1 = 31 * result1 + input.hashCode();
+            return result1;
+        }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    private static class Error<O> extends ParseResult<O> {
+        private final String message;
+        private final String input;
 
-        Ok<?> that = (Ok<?>) o;
+        public Error(String message, String remainingInput) {
+            this.message = message;
+            this.input = remainingInput;
+        }
 
-        if (!result.equals(that.result)) return false;
-        return input.equals(that.input);
-    }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
 
-    @Override
-    public int hashCode() {
-        int result1 = result.hashCode();
-        result1 = 31 * result1 + input.hashCode();
-        return result1;
-    }
-}
+            Error<?> that = (Error<?>) o;
 
-class Error<O> extends ParseResult<O> {
-    private final String message;
-    private final String input;
+            if (!message.equals(that.message)) return false;
+            return input.equals(that.input);
+        }
 
-    public Error(String message, String remainingInput) {
-        this.message = message;
-        this.input = remainingInput;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Error<?> that = (Error<?>) o;
-
-        if (!message.equals(that.message)) return false;
-        return input.equals(that.input);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = message.hashCode();
-        result = 31 * result + input.hashCode();
-        return result;
+        @Override
+        public int hashCode() {
+            int result = message.hashCode();
+            result = 31 * result + input.hashCode();
+            return result;
+        }
     }
 }
